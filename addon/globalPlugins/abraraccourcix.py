8cc736c90b268d418f5458a7ccb0e1e58885af14
Patrick ZAJDA <patrick@zajda.fr>
2015-12-26 22:58:33 +0100

    Detect if the window was really closed.

--- addon/globalPlugins/abraraccourcix.py
+++ addon/globalPlugins/abraraccourcix.py
@@ -7,6 +7,7 @@
 # Shortcut: pause
 
 import globalPluginHandler, addonHandler
+import api
 import ui
 import wx
 from keyboardHandler import KeyboardInputGesture
@@ -14,14 +15,31 @@ from keyboardHandler import KeyboardInputGesture
 # We initialize translation support
 addonHandler.initTranslation()
 
+def getWindowTitleAndAppModule():
+	""" Return the current window title and the corresponding appModule """
+	obj=api.getForegroundObject()
+	title=obj.name
+	if not isinstance(title,basestring) or not title or title.isspace():
+		title=obj.appModule.appName  if obj.appModule else "No title"
+	return title, obj.appModule if obj.appModule else "No appModule"
+
+def checkNewWindow(title, appModule):
+		newTitle, newAppModule = getWindowTitleAndAppModule()
+		if newTitle == title and newAppModule == appModule:
+			# Translators: message announced if the window was probably not closed
+			ui.message(_(u"Window probably not closed"))
+		else :
+			# Translators: confirmation message when Alt+F4 has been performed
+			ui.message(_("Window closed"))
+
 class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 	# We initialize the scripts category shown on input gestures dialog
 	scriptCategory = u"Abraraccourcix"
 
 	def script_pauseFermer(self, gesture):
+		title,appModule = getWindowTitleAndAppModule()
 		KeyboardInputGesture.fromName("alt+f4").send()
-		# Translators: confirmation message when Alt+F4 has been performed
-		wx.CallLater(100,ui.message, _("Window closed"))
+		wx.CallLater(100, checkNewWindow, title, appModule)
 	# Translators: message presented when user performes input help for the pose key script
 	script_pauseFermer.__doc__ = _(u"Close the focused window")
 
